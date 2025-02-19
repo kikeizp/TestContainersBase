84ebce8e94853a85121018aa9f948d5afa348ce3
Enrique Izquierdo <eizquierdo@MacBook-Pro-de-Administrador.local>
2025-02-19 16:43:02 +0100

    Add testing

--- Containers/BaseConfig.cs
+++
@@ -1,6 +0,0 @@
-namespace IntegrationTestingBase.Containers
-{
-    public record BaseConfig {
-        public string? Image;
-    }
-}
--- Core/Interfaces/IConfigurableTestFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace IntegrationTestingBase.Core.Interfaces
-{
-    public interface IConfigurableTestFactory<TProgram> where TProgram : class
-    {
-        Task ConfigureClients();
-    }
-}
